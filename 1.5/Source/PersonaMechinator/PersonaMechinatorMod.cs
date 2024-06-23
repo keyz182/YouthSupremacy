36e21a176213ef03bf0933d5ba4c3841b41dbec9
Kieran David Evans <keyz182@gmail.com>
2024-06-23 12:32:13 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/PersonaMechinator/PersonaMechinatorMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace PersonaMechinator;
+
+public class PersonaMechinatorMod : Mod
+{
+    public static Settings settings;
+
+    public PersonaMechinatorMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from PersonaMechinator");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.PersonaMechinator.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "PersonaMechinator_SettingsCategory".Translate();
+    }
+}
